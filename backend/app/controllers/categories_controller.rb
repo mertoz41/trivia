b41084f20fdfc37a738f3e587f496a3653bd015e
mertoz41 <mertozkaynak@windowslive.com>
2020-06-12 18:06:32 -0400

    seeds done

--- backend/app/controllers/categories_controller.rb
+++ backend/app/controllers/categories_controller.rb
@@ -1,2 +1,8 @@
 class CategoriesController < ApplicationController
+    def index
+        render json: Category.all.to_json(
+            :except => [:created_at, :updated_at])
+
+
+    end 
 end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -8,38 +8,28 @@
 
 require 'rest-client'
 
-
-
-trivia = RestClient.get 'https://opentdb.com/api.php?amount=25&type=multiple'
-trivia_array = JSON.parse(trivia)['results']
-category = trivia_array.map {|i| i['category']}
-category.map {|i| Category.create(name: i)}
-
-question = trivia_array.map{|i| i['question']}
-
-
-
-correct_choices = trivia_array.map{|i| i['correct_answer']}
-incorrect_choices = trivia_array.map{|i| i['incorrect_answers']}
-byebug
-
-
-
-
-
 User.destroy_all
 Category.destroy_all
 Question.destroy_all
 Choice.destroy_all
 
 
+trivia = RestClient.get 'https://opentdb.com/api.php?amount=25&type=multiple'
+trivia_array = JSON.parse(trivia)['results']
 
-# user1 = User.create(name: Faker::Name.unique.name, username: Faker::FunnyName.name, password: "12345", high_score:0)
-# user2 = User.create(name: Faker::Name.unique.name, username: Faker::FunnyName.name, password: "12345", high_score:0)
-# mert = User.create(name: "Mert", username: "MERTY", password: "1234", high_score: 10)
-# categ1 = Category.create(name: "Landmarks")
-# ques1 = Question.create(text: "How tall is the Eiffel tower?", category_id: categ1.id, difficulty: "medium")
-# choice1 = Choice.create(text: "10 feet", correct: false, question_id: ques1.id)
-# choice2 = Choice.create(text: "1063 feet", correct: true, question_id: ques1.id)
-# choice3 = Choice.create(text: "1000 feet", correct: false, question_id: ques1.id)
-# choice4 = Choice.create(text: "3000 feet", correct: false, question_id: ques1.id)
+trivia_array.each do |question_hash|
+    
+    category = Category.find_or_create_by(name: question_hash["category"])
+    question = Question.create(text: question_hash["question"], category_id: category.id, difficulty: question_hash["difficulty"])
+    correct_choice = Choice.create(text: question_hash["correct_answer"], question_id: question.id, correct: true)
+    question_hash['incorrect_answers'].each do |answer|
+        incorrect_choice = Choice.create(text: answer, question_id: question.id, correct: false)
+    end
+end
+
+
+
+
+
+mert = User.create(name: "Mert", username: "MERTY", password: "1234", high_score: 10)
+
