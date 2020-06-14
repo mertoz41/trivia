52ff0f66cca330052a0dde9ef73e8ad8285e674f
mertoz41 <mertozkaynak@windowslive.com>
2020-06-14 13:43:36 -0400

    frontend commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,52 @@
+const category_url = 'http://localhost:3000/categories'
+
 document.addEventListener('DOMContentLoaded', function(){
-    console.log("WORKING")
-    login()
+    document.getElementById('login-form').addEventListener('submit', handleLogin)
 })
 
-function login(){
-    let formDiv = document.getElementById('login-form')
-    let container = document.getElementById('container')
-    let button = document.createElement('button')
-    
-    button.innerHTML = "Log in"
+function handleLogin(e){
+    e.preventDefault()
+    document.getElementById('login-form').hidden = true
 
-
-
-    container.appendChild()
-
+    fetchCategories()
+    renderDifficulty()
 }
+
+
+function fetchCategories(){
+    let form = document.getElementById('category-form')
+    fetch(category_url)
+    .then(resp => resp.json())
+    .then(data => {
+        data.forEach(category => {
+            
+            renderCategory(category)
+        })
+    })
+}
+
+function renderCategory(category){
+    let select = document.getElementById('category-select')
+    let form = document.getElementById('category-form')
+    let option = document.createElement('option')
+    option.id = category.id
+    option.text = category.name
+    select.add(option)
+    form.appendChild(select)
+    form.hidden = false 
+    
+    
+    
+}
+
+function postCategory(e, id){
+    console.log("Working")
+    debugger 
+}
+
+function renderDifficulty(){
+    let form = document.getElementById('difficulty-form')
+}
+
+
+
