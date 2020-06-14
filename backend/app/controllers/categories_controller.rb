6af1d96966bdca609197be19ab6575eed705ebae
saimaja <saimaakhtar@gmail.com>
2020-06-14 14:56:57 -0400

    at render difficulty

--- backend/app/controllers/categories_controller.rb
+++ backend/app/controllers/categories_controller.rb
@@ -2,7 +2,11 @@ class CategoriesController < ApplicationController
     def index
         render json: Category.all.to_json(
             :except => [:created_at, :updated_at])
-
-
     end 
+
+    def show
+        category = Category.find(params[:id])
+        render json: category, include: :questions
+
+    end
 end
