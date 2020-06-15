53cc32a831a72b87aa377578df26723e906aa322
mertoz41 <mertozkaynak@windowslive.com>
2020-06-15 11:23:48 -0400

    working on difficulty dropdown

--- backend/app/controllers/questions_controller.rb
+++ backend/app/controllers/questions_controller.rb
@@ -3,4 +3,9 @@ class QuestionsController < ApplicationController
         questions = Question.all
         render json: questions
     end 
+
+    def by_category_and_difficulty
+        questions = # some method that returns questions based on params category_Id and difficulty
+        
+    end
 end
--- backend/app/models/question.rb
+++ backend/app/models/question.rb
@@ -1,4 +1,8 @@
 class Question < ApplicationRecord
     has_many :choices
     belongs_to :category
+
+    # def difficulty
+    #     self.difficulty
+    # end
 end
