4f75d242dad54543c76e840cc59208e98e90ce75
saimaja <saimaakhtar@gmail.com>
2020-06-17 11:01:28 -0400

    leader board

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+    def index
+        render json: User.all.to_json(
+            :except => [:username, :password, :created_at, :updated_at]
+        )
+    end 
+
+    
+    def create
+        new_user = User.create(name: params["name"], high_score: params["high_score"])
+        render json: new_user.to_json(
+            :except => [:username, :password, :created_at, :updated_at]
+        )       
+    end 
+
 end
